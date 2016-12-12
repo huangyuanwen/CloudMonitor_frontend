1127ad0a4a1232f9a4ed6b07127c65895bbf096c
Me1kro <191926083@qq.com>
2016-12-12 14:43:57 +0800

    v0.1

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,7 @@ module.exports = {
     entry: __dirname + '/src/main.js',
     output: {
         path: path.resolve(__dirname, './dist'),
+        // publicPath: '/dist/',
         filename: '[name].[hash].build.js'
     },
     module: {

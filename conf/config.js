49c99817fc744cc8eb678900d7ec4a2b35ff76ea
Me1kro <191926083@qq.com>
2017-01-05 10:05:00 +0800

    v0.1

--- conf/config.js
+++ conf/config.js
@@ -1,11 +1,10 @@
 /**
  * Created by Me1kro on 16/12/15.
  */
+import Env from './env';
 module.exports = {
-    porxy:{
-        target: 'http://10.7.73.86:8080', //连接的服务器地址
-        // base:"./CloudMonitor",//监听地址
-        base:".",//监听地址
+    porxy: {
+        base: Env != 'production' ? './CloudMonitor' : '.'
     }
 
 };
